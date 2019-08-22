9088c667435896d81b66158c00f1b8b3bcac094a
Aman Agarwal <aman.agarwal@spirent.com>
2019-08-22 11:09:02 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const per = require('./libs/pbs/person_pb');
+
+let person = new per.Person();
+/*
+var data={
+	name:"john, doe",
+	id:12345,
+	email:'john.doe@example.com',
+	phone:[{number:'11111111', type:0}, {number:'111112222', type:1}]
+}*/
+
+//on Sender side:Send Data over the wire
+person.setName("john, doe")
+person.setId("12345");
+person.setEmail("john.doe@example.com");
+let bufPerson = person.serializeBinary()
+console.log("=========Data sent========");
+
+console.log(".");
+console.log(".");
+console.log(".");
+console.log(".");
+console.log(".");
+console.log(".");
+
+//on Receiver side: Receive Data over the wire
+console.log("=========Data Received========");
+let desPerson = per.Person.deserializeBinary(bufPerson);
+const personObj = desPerson.toObject();
+console.log(`Name ${person.getName()}`);
+console.log(`Id ${person.getId()}`)
+console.log(`Email ${person.getEmail()}`)
